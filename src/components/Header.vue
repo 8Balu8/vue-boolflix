3d54edc90f6c3707e09ae6aa7b6adcc3057dab3a
8Balu8 <barzagliluca@gmail.com>
2022-01-11 15:45:17 +0100

    scaffolding

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <input type="text" placeholder="Title">
+        <button>Search</button>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    height: 50px;
+    background-color: gray;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<template>
+<main>
+    <div class="container">
+        <div class="card-contain">
+            <div class="card">
+                <ul>
+                    <li>title</li>
+                    <li>title</li>
+                    <li>title</li>
+                    <li>title</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</main>
+</template>
+
+<script>
+export default {
+    name: "Main"
+}
+</script>
+
+<style lang="scss" scoped>
+main {
+    padding: 50px;
+
+    .card-contain {
+        display: flex;
+        flex-wrap: wrap;
+
+        .card {
+            width: calc((100% / 3) - 40px);
+            margin: 10px 20px;
+        }
+    }
+}
+</style>
